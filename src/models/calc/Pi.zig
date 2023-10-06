6f445ebbe0d03a7e7d679ce9275e1716b148d6b4
Rodrigo Garcia <rodrigo@rgnu.com.ar>
2023-10-06 15:29:57 -0500

    Add Pi Operator

---
+++ src/models/calc/Pi.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const apis = @import("../../apis/main.zig");
+
+const Self = @This();
+
+pub fn init() Self {
+    const self = Self{};
+
+    return self;
+}
+
+//Implement Operator interface
+pub fn operator(self: *const Self) apis.calc.Operator {
+    return apis.calc.Operator.init(self);
+}
+
+pub fn eval(_: *const Self) f32 {
+    return 3.141516;
+}
+
+test "models/calc/Pi.init" {
+    const testing = std.testing;
+    const o = Self.init();
+
+    try testing.expectEqual(@as(anyerror!f32, 3.141516), o.eval());
+}
--- src/models/calc/main.zig
+++ src/models/calc/main.zig
@@ -4,6 +4,7 @@ pub const Value = @import("Value.zig");
 pub const Plus = @import("Plus.zig");
 pub const Minus = @import("Minus.zig");
 pub const Multiply = @import("Multiply.zig");
+pub const Pi = @import("Pi.zig");
 
 test "models/calc" {
     std.testing.refAllDecls(@This());
